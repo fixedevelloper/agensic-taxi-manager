58840e490f60c211dad30f705db00e723b3bc276
rodriguembah13 <rodriguembah13@gmail.com>
2022-12-13 09:45:07 +0100

    fcommit

--- src/Controller/Api/StaticApiController.php
+++ src/Controller/Api/StaticApiController.php
@@ -910,7 +910,7 @@ class StaticApiController extends AbstractFOSRestController
         return $this->handleView($view);
     }
     /**
-     * @Rest\Get("/v1/shippings/{id}", name="api_shippings_one")
+     * @Rest\Get("/v1/shippings/{id}/one", name="api_shippings_one")
      * @param Request $request
      * @return Response
      */
@@ -993,7 +993,7 @@ class StaticApiController extends AbstractFOSRestController
     }
 
     /**
-     * @Rest\Get("/v1/shippings/driver/{driver}/list", name="api_shippings_driver")
+     * @Rest\Get("/v1/shippings/driver/{driver}", name="api_shippings_driver")
      * @param Request $request
      * @param Driver $driver
      * @return Response
